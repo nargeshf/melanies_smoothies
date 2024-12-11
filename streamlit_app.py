8d3fd04d132e7edb26a26d7a8c56481580f35c15
nargeshf <137073058+nargeshf@users.noreply.github.com>
2024-12-11 14:54:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,48 @@
-insert into smoothies.public.orders(ingredients)
-values ('Cantaloupe Guava Jackfruit Elderberries Figs ');
-
-select * from smoothies.public.orders;
-
-truncate table smoothies.public.orders;
-
-alter table SMOOTHIES.PUBLIC.ORDERS ADD column ORDER_FILLED BOOLEAN DEFAULT FALSE;
-
-
-       update smoothies.public.orders
-       set order_filled = true
-       where name_on_order is null;
-
-
-insert into smoothies.public.orders(ingredients,NAME_ON_ORDER) values ('Dragon Fruit Honeydew Guava Apples Kiwi','MellyMel');
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
 
 
 
-truncate table SMOOTHIES.PUBLIC.ORDERS;
-
-alter table SMOOTHIES.PUBLIC.ORDERS
-add column order_uid integer --adds the column
-default smoothies.public.order_seq.nextval --sets the value of the column to sequence
-constraint order_uid unique enforced; --makes sure there is always a unique value in the column
-
-
-
-og_dataset = session.table("smoothies.public.orders")
-    edited_dataset = session.create_dataframe(editable_df)
-    og_dataset.merge(edited_dataset
-                     , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
-                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+    """Choose the fruits you want in your custom Smoothie!
+    """
+)
 
 
-                    );
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
 
 
-select * from ORDERS;
-alter table fruit_options add column SEARCH_ON TEXT;
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+st.dataframe(data=my_dataframe, use_container_width=True)
 
--- copy name to seed the columns - then we can just edit the problem rows
-update fruit_options
-set search_on = SEARCH_ON;
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+)
+if ingredients_list:
+    ingredients_string = ''
 
-select * from fruit_options;
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
 
-update fruit_options
-set search_on = 'Apple'
-where SEARCH_ON = 'Apples';
+#st.write(ingredients_string)
+
+my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
+#st.write(my_insert_stmt)
+#st.stop()
+
+#st.write(my_insert_stmt)
+time_to_insert = st.button('Submit Order')
+
+if time_to_insert:
+    session.sql(my_insert_stmt).collect()
+
+    st.success('Your Smoothie is ordered!',icon="✅")
