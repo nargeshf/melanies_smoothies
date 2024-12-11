8b8c9e04ddb7c6aa1ef982002f8be56bf85bdf6a
nargeshf <137073058+nargeshf@users.noreply.github.com>
2024-12-11 14:39:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/wa
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+#pd_df = my_dataframe.to_pandas()
+#st.dataframe(pd_df)
 #st.stop()
 
 
