bcfcb692783e94906b9f6105c716f41a0f4e5600
nargeshf <137073058+nargeshf@users.noreply.github.com>
2024-12-11 14:53:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=Tru
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-# st.dataframe(pd_df)
+st.dataframe(pd_df)
 # st.stop()
 
 ingredients_list = st.multiselect(
