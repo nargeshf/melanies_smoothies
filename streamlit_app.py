10acb6b1a032063f4425491338d8bb078f6429bc
nargeshf <137073058+nargeshf@users.noreply.github.com>
2024-12-11 14:28:11 -0500

    Update streamlit_app.py
    back

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,19 +36,22 @@ ingredients_list = st.multiselect(
 #st.write(ingredients_string)
 
 ingredients_string = ''
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-if ingredients_list:
+#if ingredients_list:
     #ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+    #for fruit_chosen in ingredients_list:
+      #  ingredients_string += fruit_chosen + ' '
 
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+       # search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+       # st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        #st.subheader(fruit_chosen + ' Nutrition Information')
+        #fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 # st.write(ingredients_string)
 
