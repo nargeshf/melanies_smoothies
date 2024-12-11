dd18456f7bc789c1afe57c92b5125d1cc8ab1c30
nargeshf <137073058+nargeshf@users.noreply.github.com>
2024-12-11 12:52:36 -0500

    Update streamlit_app.py
    If uncomment

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,21 +31,18 @@ ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
 )
-#if ingredients_list:
- #   ingredients_string = ''
+if ingredients_list:
+    ingredients_string = ''
 
-  #  for fruit_chosen in ingredients_list:
-   #     ingredients_string += fruit_chosen + ' '
-    #    st.subheader(fruit_chosen + ' Nutrition Information')
-     #   smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-      #  sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+   for fruit_chosen in ingredients_list:
+      ingredients_string += fruit_chosen + ' '
+       st.subheader(fruit_chosen + ' Nutrition Information')
+       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 #st.write(ingredients_string)
 
-# New section to display smoothiefroot nutrition information
-#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 #if ingredients_list:
     #ingredients_string = ''
