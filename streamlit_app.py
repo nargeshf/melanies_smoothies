bc1d7746b1df837636825a1dcd07cf4d07e29f58
nargeshf <137073058+nargeshf@users.noreply.github.com>
2024-12-11 12:45:40 -0500

    Update streamlit_app.py
    remove ifs

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,14 +31,14 @@ ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
 )
-if ingredients_list:
-    ingredients_string = ''
+#if ingredients_list:
+ #   ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+  #  for fruit_chosen in ingredients_list:
+   #     ingredients_string += fruit_chosen + ' '
+    #    st.subheader(fruit_chosen + ' Nutrition Information')
+     #   smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+      #  sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 #st.write(ingredients_string)
@@ -47,8 +47,8 @@ if ingredients_list:
 #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-if ingredients_list:
-    ingredients_string = ''
+#if ingredients_list:
+    #ingredients_string = ''
 
     #for fruit_chosen in ingredients_list:
        # ingredients_string += fruit_chosen + ' '
