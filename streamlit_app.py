bc6fe30010f93f21fdc749c81eb87cd840be2e26
nargeshf <137073058+nargeshf@users.noreply.github.com>
2024-12-11 12:03:19 -0500

    Update streamlit_app.py
    Move section nutritions

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-# New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 # Write directly to the app
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -38,15 +32,21 @@ if ingredients_list:
 
 #st.write(ingredients_string)
 
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
 
 #st.write(my_insert_stmt)
 #st.stop()
 
 #st.write(my_insert_stmt)
 time_to_insert = st.button('Submit Order')
-
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
 
