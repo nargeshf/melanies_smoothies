26cea078587742e7fe30eae519d26cf0fc2452cc
nargeshf <137073058+nargeshf@users.noreply.github.com>
2024-12-11 11:49:11 -0500

    Update streamlit_app.py
    # New section to display smoothiefroot nutrition information
    import requests - website

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 # Write directly to the app
 
